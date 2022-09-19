bd3d54fa7b7cb666b06aa7378c14fd550cba4a2e
tsunN <50802365+Tsun0193@users.noreply.github.com>
2022-09-20 01:41:45 +0700

    Initializing HUD. Working on Playing Display.

--- core/src/com/bomman/game/display/playDP.java
+++ core/src/com/bomman/game/display/playDP.java
@@ -1,9 +1,11 @@
 package com.bomman.game.display;
 
+import com.artemis.BaseSystem;
 import com.artemis.WorldConfiguration;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -12,6 +14,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.bomman.game.BGame;
 import com.bomman.game.game.gameManager;
+import com.bomman.game.gui.hud;
 
 public class playDP extends ScreenAdapter {
     private final BGame bGame;
@@ -21,6 +24,7 @@ public class playDP extends ScreenAdapter {
     private int mapWidth;
     private int mapHeight;
     private int level;
+    private float box2dTimer;
     private final float WIDTH = 20;
     private final float HEIGHT = 15;
     private boolean pauseFlag;
@@ -32,6 +36,9 @@ public class playDP extends ScreenAdapter {
 
     private Box2DDebugRenderer box2DRenderer;
     private boolean box2DRendererFlag;
+
+
+    private hud Hud;
 
     /**
      * Constructor.
@@ -61,8 +68,12 @@ public class playDP extends ScreenAdapter {
         world = new com.artemis.World(worldCfg);
 
         gameManager.setEnemiesLeft(0);
-        gameManager.setGameFinished(true);
-        gameManager.setGameOver(true);
+        gameManager.setGameFinished(false);
+        gameManager.setGameOver(false);
+
+        Hud = new hud(batch, WIDTH, HEIGHT);
+        Hud.setLevelInfo(level);
+        box2dTimer = 0;
 
         switch (level) {
             default:
@@ -79,6 +90,44 @@ public class playDP extends ScreenAdapter {
                 break;
         }
         pauseFlag = false;
+    }
+
+    public void render(float f) {
+        inputHandle();
+
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.end();
+
+        if (!pauseFlag) {
+            box2dTimer += f;
+            if (box2dTimer > 1 / 60.0f) {
+                box2DWorld.step(1 / 60.0f, 8, 3);
+                box2dTimer -= 1 / 60.0f;
+            }
+
+            for (BaseSystem sys : world.getSystems()) {
+                sys.setEnabled(true);
+            }
+        } else {
+            for (BaseSystem sys : world.getSystems()) {
+//                if (!(sys instanceof RenderSystem)) {
+                    sys.setEnabled(false);
+//                }
+            }
+        }
+
+        world.setDelta(f);
+        world.process();
+
+        Hud.draw(f);
+
+        if (box2DRendererFlag) {
+            box2DRenderer.render(box2DWorld, camera.combined);
+        }
     }
 
     @Override
---
+++ core/src/com/bomman/game/gui/hud.java
@@ -0,0 +1,83 @@
+package com.bomman.game.gui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Disposable;
+import com.bomman.game.game.gameManager;
+
+public class hud implements Disposable {
+    private final SpriteBatch batch;
+    private TextureAtlas textureAtlas;
+    private Texture bgTexture;
+    private Texture bombTimerTexture;
+
+    private Sprite bombSprite;
+    private Sprite bombTimerSprite;
+    private Sprite powerSprite;
+    private Sprite speedSprite;
+    private Sprite remoteSprite;
+    private Sprite bommanSprite;
+
+    private Label fpsLabel;
+    private Label lvLabel;
+    private Label livesLabel;
+    private Label xLabel;
+    private Label zLabel;
+
+    private boolean showFPS = false;
+    private StringBuilder stringBuilder;
+
+
+    public hud(SpriteBatch batch, float width, float height){
+        this.batch = batch;
+        AssetManager assetManager = gameManager.getInstance().getAssetManager();
+        textureAtlas = assetManager.get("img/actors.pack", TextureAtlas.class);
+        bombSprite = new Sprite(new TextureRegion(textureAtlas.findRegion("Bomb"), 0, 0, 16, 16));
+        bombSprite.setBounds(15.0f, 11.5f, 1, 1);
+
+        Pixmap pixmap = new Pixmap(5, 15, Pixmap.Format.RGBA8888);
+        pixmap.setColor(240.0f / 255.0f, 128 / 255.0f, 0, 1.0f);
+        pixmap.fill();
+
+        bgTexture = new Texture(pixmap);
+
+        pixmap.setColor(1, 1, 1, 1);
+        pixmap.fill();
+        bombTimerTexture = new Texture(pixmap);
+        pixmap.dispose();
+
+
+        stringBuilder = new StringBuilder();
+    }
+
+    public void draw(float f) {
+        handleInput();
+        batch.begin();
+        batch.end();
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F)) {
+            showFPS = !showFPS;
+            fpsLabel.setVisible(showFPS);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        bgTexture.dispose();
+        bombTimerTexture.dispose();
+    }
+
+    public void setLevelInfo(int lv) {
+        lvLabel.setText("Level: " + lv);
+    }
+}
